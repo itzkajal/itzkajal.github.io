54cd06048035dcf502e4c500dfa066d27c2bf911
Kajal Rajendra Jawale <121432770+itzkajal@users.noreply.github.com>
2026-02-19 15:48:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const roles = ["Full Stack Developer", "Cloud & DevOps Engineer", "Backend Specialist"];
+const roles = [
+  "Full Stack Developer",
+  "Cloud & DevOps Engineer",
+  "Backend Specialist"
+];
+
 let roleIndex = 0;
 let charIndex = 0;
 let typingElement = document.querySelector(".typing");
 
 function type() {
-if (charIndex < roles[roleIndex].length) {
-typingElement.textContent += roles[roleIndex].charAt(charIndex);
-charIndex++;
-setTimeout(type, 100);
-} else {
-setTimeout(erase, 2000);
-}
+  if (charIndex < roles[roleIndex].length) {
+    typingElement.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 80);
+  } else {
+    setTimeout(erase, 1500);
+  }
 }
 
 function erase() {
-if (charIndex > 0) {
-typingElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
-charIndex--;
-setTimeout(erase, 50);
-} else {
-roleIndex = (roleIndex + 1) % roles.length;
-setTimeout(type, 500);
-}
+  if (charIndex > 0) {
+    typingElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 40);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(type, 300);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-type();
+  type();
 });
